7b671770efe5d91d9e0d3df7229d4ac7e3d8d9f6
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-09-08 14:18:21 -0500

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+import Card from './components/Card';
+import Form from './components/Form';
+import './css/styles.css';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <>
+      <main className="main-container">
+        <Card/>
+        <Form/>
+      </main>
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/UrielBucio">Uriel Bucio</a>.
+      </footer>
+    </>
   )
 }
 
