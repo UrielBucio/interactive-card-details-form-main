ed393cfc58b69ee86911f5221e9b9a69629f6cc3
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-09-09 13:53:00 -0500

    Send props children

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 import './css/styles.css';
 
 function App() {
-
+  const [cardForm, setCardForm] = useState({});
+  
   return (
     <>
       <main className="main-container">
-        <Card/>
-        <Form/>
+        <Card cardForm={cardForm}/>
+        <Form setCardForm={setCardForm}/>
       </main>
       <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
