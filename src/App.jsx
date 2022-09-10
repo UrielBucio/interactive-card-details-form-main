35897d053ad73806445ef0bc92f91b34196f84f2
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-09-10 15:48:20 -0500

    Conditional rendering form and complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { useState} from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
+import Thanks from './components/Thanks';
 import './css/styles.css';
 
 function App() {
   const [cardForm, setCardForm] = useState({});
+  const [complete, setComplete] = useState(false);
   
   return (
     <>
       <main className="main-container">
         <Card cardForm={cardForm}/>
-        <Form setCardForm={setCardForm}/>
+        { complete ? <Thanks setComplete={setComplete}/> : <Form setCardForm={setCardForm} setComplete={setComplete}/>}
       </main>
       <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
